c972b413feed3111ab466b1f3d1a9df3cd8d32f1
Dirk Eddelbuettel <edd@debian.org>
2025-06-23 16:58:47 -0500

    Corrections to checkRelease.r

--- pkg/checkRelease.r
+++ pkg/checkRelease.r
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+setwd("~/git/duck-deb/pkg")
+
 url <- "https://api.github.com/repos/duckdb/duckdb/releases"
 res <- jsonlite::fromJSON(url)
 
@@ -10,13 +12,14 @@ chnglg <- chnglg[grepl("^duckdb \\(.*\\).*", chnglg)]
 pkgs <- gsub(".*\\((.*)\\).*", "\\1", chnglg)
 
 if (releases[1] == pkgs[1])
-    q(ask="no")
+    q(save="no")
 
 tab <- res$assets[[1]]
 files <- tab$browser_download_url
 zipfile <- files[grepl("cli-linux-amd64.zip$", files)]
 tgt <- basename(zipfile)
-if !file.exists(tgt) {
+
+if (!file.exists(tgt)) {
     download.file(zipfile, tgt, quiet=TRUE)
     cat("Downloaded", tgt, "\n")
 }
@@ -31,4 +34,12 @@ system(cmd)
 cmd <- "dpkg-buildpackage -us -uc -rfakeroot -tc"
 system(cmd)
 
-unlink(zipfile)
+unlink(tgt)
+
+newdeb <- paste0("../duckdb_", releases[1], "_amd64.deb")
+if (file.exists(newdeb))
+    res <- file.copy(newdeb, "~/git/ppa-rstudio/docs")
+
+olddeb <- paste0("../duckdb_", pkgs[1], "_amd64.deb")
+if (file.exists(olddeb))
+    res <- file.remove(olddeb)
